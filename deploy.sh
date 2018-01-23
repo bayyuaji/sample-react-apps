228683187964bfc0f8cde56647a6bd06028e1ff4
rawis <rawis@sepulsa.com>
2018-01-23 16:08:41 +0700

    modify deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #coba
 #!/bin/sh      
 ssh admin@52.221.79.145 <<EOF        
- cd /home/admin/sepulsa/jenkins-test 
- git pull       
- yarn build
+ cd /home/admin/sepulsa/jenkins-test && \
+ git pull && \
+ npm install && \      
+ yarn build && \
  exit
 EOF
